d8524cc326f66186bc2b4a8a5af2a64b0a1e4f61
Rainel Urdaneta <gerencia@garzonromero.com>
2023-11-22 22:45:43 -0300

    .

--- resources/views/mina/empresa/viaje/tablaBoton.blade.php
+++ resources/views/mina/empresa/viaje/tablaBoton.blade.php
@@ -1,8 +1,5 @@
 @can('Viaje editar')
 	<a class="btn btn-xs btn-info" href="{{ route('viaje.editar', $id) }}"><i class="fas fa-edit"></i> Editar</a>
-	<button type="button" class="btn btn-xs btn-success" data-toggle="modal" data-target="#exampleModal" onclick="changeId({{ $id }})">
-		<i class="fas fa-envelope"></i> Enviar Certificado Origen
-	</button>
 @endcan
 @can('Viaje borrar')
 	@if($activo==1)
@@ -10,4 +7,9 @@
 	@else
 		<a class="btn btn-xs btn-info" href="{{ route('viaje.activar', $id) }}"><i class="fas fa-check-circle"></i> Activar</a>
 	@endif
+@endcan
+@can('Viaje editar')
+	<button type="button" class="btn btn-xs btn-success" data-toggle="modal" data-target="#exampleModal" onclick="changeId({{ $id }})">
+		<i class="fas fa-envelope"></i> Enviar Certificado Origen
+	</button>
 @endcan
